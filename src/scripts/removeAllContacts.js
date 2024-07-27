d9a1f09f5a99263c3aaf4a18929b99da810798c8
Yuliia7777 <yuliiakorobkova1@gmail.com>
2024-07-27 14:13:01 +0200

    first

--- src/scripts/removeAllContacts.js
+++
@@ -1,8 +0,0 @@
-import { writeContactsToFile } from '../utils/fileOperations.js';
-
-export const removeAllContacts = async () => {
-  writeContactsToFile([]);
-};
-
-removeAllContacts();
-console.log('All contacts have been removed.');
